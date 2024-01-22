1c8dc0a64533ae3cd2918a04031a976ef902c39b
HigorMonteiro <higorvmonteiro@gmail.com>
2024-01-22 11:37:58 -0300

    feature: Add action to break the cookie and custom animated layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,35 @@
 import { useState } from 'react'
-import cookie from './assets/cookie.svg'
-import viteLogo from '/vite.svg'
+import cookie from './assets/fortune-cookie.png'
+import openCookie from './assets/opened-cookie.png'
+import fortuneIcon from './assets/trevo-icon.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isCookieOpened, setCookieOpened] = useState(false)
+
+  const handleCookieClick = () => {
+    setCookieOpened(true);
+    document.body.classList.add('shake');
+    setTimeout(() => document.body.classList.remove('shake'), 500);
+  }
+
+  const handleNewCookieClick = () => {
+    setCookieOpened(false);
+  }
 
   return (
     <>
       <div>
-        <a href="#" target="">
-          <img src={cookie} className="logo" alt="Cookie image" />
+        <a href="#" target="" onClick={handleCookieClick}>
+          <img src={isCookieOpened ? openCookie : cookie} className={isCookieOpened ? "opened-logo" : "logo"} alt="Cookie image" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      {isCookieOpened && <button onClick={handleNewCookieClick}>Try a new fortune cookie <img src={fortuneIcon} alt="Fortune icon" /></button>}
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          {isCookieOpened && 'You will find a fortune cookie'}
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
